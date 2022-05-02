36e0b6d019350c76d9fd4fad15602b40979c1ce6
Pariterre <pariterre@hotmail.com>
2022-05-02 09:26:09 -0400

    Removed HeaderData

--- Assets/Scripts/DataHandler.cs
+++ Assets/Scripts/DataHandler.cs
@@ -10,8 +10,6 @@ using UnityEngine.Networking;
 /// </summary>
 public class DataHandler : MonoBehaviour
 {
-	HeaderData headerData;
-
 	private string pid;
 
 	int difficultyEvaluationIndex = -1;
@@ -62,38 +60,6 @@ public class DataHandler : MonoBehaviour
 		pid = GlobalControl.Instance.participantID + "_" + now.Month.ToString() + "-" + now.Day.ToString() + "-" + now.Year + "_" + now.Hour + "-" + now.Minute + "-" + now.Second; // + "_" + pid;
 
 	}
-
-	public void recordHeaderInfo(SessionType.Session s, int maxtime, float htime, float twidth)
-	{
-		headerData = new HeaderData(s, maxtime, htime, twidth);
-	}
-
-	private void WriteHeaderInfo(CsvFileWriter writer)
-	{
-		CsvRow s = new CsvRow();
-		s.Add("Session");
-		s.Add(headerData.session.ToString());
-
-		CsvRow maxtime = new CsvRow();
-		maxtime.Add("Time Limit (s)");
-		maxtime.Add(headerData.maxTrialTimeMin.ToString());
-
-		CsvRow hovertime = new CsvRow();
-		hovertime.Add("Ball Hover Time (s)");
-		hovertime.Add(headerData.hoverTime.ToString());
-
-		CsvRow trad = new CsvRow();
-		trad.Add("Target Line Acceptance Width (m)");
-		trad.Add(headerData.targetWidth.ToString());
-
-		writer.WriteRow(s);
-		writer.WriteRow(maxtime);
-		writer.WriteRow(hovertime);
-		writer.WriteRow(trad);
-		writer.WriteRow(new CsvRow());
-	}
-
-
 
 	// utility functions --------------------------------------------
 
