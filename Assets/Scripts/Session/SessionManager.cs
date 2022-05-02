9bb3c54fdd3be0b768052d7ddbe977a8fc0d402c
Pariterre <pariterre@hotmail.com>
2022-05-02 10:31:14 -0400

    Preparing to change "sessionManager" to "difficultyManager"

--- Assets/Scripts/Session/SessionManager.cs
+++ Assets/Scripts/Session/SessionManager.cs
@@ -1,79 +1,25 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
 public class SessionManager : MonoBehaviour
 {
-    [SerializeField, Tooltip("The paddles manager")]
-    private PaddlesManager paddlesManager;
-
-    #region Interface
-    public int nbOfBounceRequired { get { return currentDifficulty.nbOfBounceRequired; } }
-    public int nbOfAccurateBounceRequired { get { return currentDifficulty.nbOfAccurateBounceRequired; } }
-    public bool hasTarget { get { return currentDifficulty.shouldShowTarget(currentLevel); } }
-    public TargetEnum.Height targetHeight { get { return GlobalControl.Instance.targetHeightPreference; } }
-    public float targetHeightOffset { get { return currentDifficulty.targetHeightOffset(currentLevel); } }
-    public float targetWidth { get { return currentDifficulty.targetWidth(currentLevel); } }
-    public float ballSpeed { get { return currentDifficulty.ballSpeed(currentLevel); } }
-    public bool isTimeOver(double elapsedTime) { return elapsedTime > maximumTrialTime; }
+    #region Functions to move out of the file
+    public bool isTimeOver(double elapsedTime) { return elapsedTime > currentDifficulty.maximumTrialTime; }
     public bool isSessionOver
     {
         get { return currentDifficultyChoiceIndex >= difficultyOverSession.Count; }
     }
-    public double maximumTrialTime { get { return currentDifficulty.maximumTrialTime; } }
-    public bool mustSwitchPaddleAfterHitting { get { return paddlesManager.NbPaddles > 1; } }
-    #endregion
 
 
-
-    #region Level
-    private int _currentLevel = 0;
-    public int currentLevel { 
-        get { return _currentLevel; }
-        set
-        {
-            if (value < 0 || value > currentDifficulty.nbLevel)
-            {
-                Debug.LogError("Issue setting difficulty, not in expected range: " + value);
-                return;
-            }
-            _currentLevel = value;
-        }
-    }
-    #endregion
-
-
-
-    #region Difficulty
-    public string difficultyName { get { return currentDifficulty.name; } }
-    List<DifficultyChoice> difficultyOverSession = new List<DifficultyChoice>()
-    {
-        DifficultyChoice.BASE,
-        DifficultyChoice.MODERATE,
-        DifficultyChoice.MAXIMAL,
-        DifficultyChoice.MODERATE
-    };
-    public void AddDifficultyToSessionList(DifficultyChoice newDifficulty)
-    {
-        difficultyOverSession.Add(newDifficulty);
-    }
-    int currentDifficultyChoiceIndex = 0;
-    DifficultyChoice currentDifficultyChoice { 
-        get { return difficultyOverSession[currentDifficultyChoiceIndex];  } 
-    }
-    DifficultyFactory difficultyFactory = new DifficultyFactory();
-    DifficultyDefinition currentDifficulty
-    {
-        get { return difficultyFactory.trialLevelDefinitions[currentDifficultyChoice]; }
-    }
-    #endregion
 
     #region Data
-    List<TrialResults> trialResults = new List<TrialResults>();
-    TrialResults currentTrial { get { return trialResults.Last(); } }
-    public void AddBounceToCurrentResults(bool _isAccurate) { 
-        currentTrial.AddBounce(_isAccurate); 
+    private List<TrialResults> trialResults = new List<TrialResults>();
+    private TrialResults currentTrial { get { return trialResults.Last(); } }
+    public void AddBounceToCurrentResults(bool _isAccurate)
+    {
+        currentTrial.AddBounce(_isAccurate);
     }
     public double EvaluateSessionPerformance(
         double _elapsedTime
@@ -102,7 +48,7 @@ public class SessionManager : MonoBehaviour
         double _averageBounces = ComputeAverage(
             _totalBounces, trialResults.Count, 0.0, 1.3
         );
-        double _averageAccurateBounces = hasTarget ? 
+        double _averageAccurateBounces = hasTarget ?
             ComputeAverage(_totalAccurateBounces, trialResults.Count, 0, 1.3) : 0;
 
         // evaluating time percentage of the way to end
@@ -123,4 +69,77 @@ public class SessionManager : MonoBehaviour
     }
     #endregion
 
+
+
+    #endregion
+
+
+
+    [SerializeField, Tooltip("The paddles manager")]
+    private PaddlesManager paddlesManager;
+
+    #region Accessors
+    #region Ball
+    public int nbOfBounceRequired { get { return currentDifficulty.nbOfBounceRequired; } }
+    public int nbOfAccurateBounceRequired { get { return currentDifficulty.nbOfAccurateBounceRequired; } }
+    public float ballSpeed { get { return currentDifficulty.ballSpeed(currentLevel); } }
+    #endregion
+
+    #region Target
+    public bool hasTarget { get { return currentDifficulty.shouldShowTarget(currentLevel); } }
+    public TargetEnum.Height targetHeight { get { return GlobalControl.Instance.targetHeightPreference; } }
+    public float targetHeightOffset { get { return currentDifficulty.targetHeightOffset(currentLevel); } }
+    public float targetWidth { get { return currentDifficulty.targetWidth(currentLevel); } }
+    #endregion
+
+    #region Trial
+    public bool mustSwitchPaddleAfterHitting { get { return paddlesManager.NbPaddles > 1; } }
+    public double maximumTrialTime { get { return currentDifficulty.maximumTrialTime; } }
+    #endregion
+    #endregion
+
+
+
+    // Level is the increment inside a difficulty condition 
+    #region Level
+    private int _currentLevel = 0;
+    public int currentLevel { 
+        get { return _currentLevel; }
+        set
+        {
+            if (value < 0 || value > currentDifficulty.nbLevel)
+            {
+                Debug.LogError("Issue setting difficulty, not in expected range: " + value);
+                return;
+            }
+            _currentLevel = value;
+        }
+    }
+    #endregion
+
+
+
+    #region Difficulty
+    public string difficultyName { get { return currentDifficulty.name; } }
+    private List<DifficultyChoice> difficultyOverSession = new List<DifficultyChoice>()
+    {
+        DifficultyChoice.BASE,
+        DifficultyChoice.MODERATE,
+        DifficultyChoice.MAXIMAL,
+        DifficultyChoice.MODERATE
+    };
+    public void AddDifficultyToSessionList(DifficultyChoice newDifficulty)
+    {
+        difficultyOverSession.Add(newDifficulty);
+    }
+    private int currentDifficultyChoiceIndex = 0;
+    private DifficultyChoice currentDifficultyChoice { 
+        get { return difficultyOverSession[currentDifficultyChoiceIndex];  } 
+    }
+    private DifficultyFactory difficultyFactory = new DifficultyFactory();
+    private DifficultyDefinition currentDifficulty
+    {
+        get { return difficultyFactory.trialLevelDefinitions[currentDifficultyChoice]; }
+    }
+    #endregion
 }
