e252469bbd66ce14307c7b44a61195b9b13e87e5
Pariterre <pariterre@hotmail.com>
2022-05-03 15:14:39 -0400

    Simplified the pause logic and added mutex

--- Assets/Scripts/Global/GlobalPauseHandler.cs
+++ Assets/Scripts/Global/GlobalPauseHandler.cs
@@ -4,7 +4,12 @@ using UnityEngine;
 
 public class GlobalPauseHandler : MonoBehaviour
 {
-    public PauseIndicator pauseIndicator;
+    [SerializeField, Tooltip("The canvas for pausing")]
+    public GameObject pauseIndicator;
+
+    // If any game object requested the indicator to be block (sort of mutex)
+    int lockKey = -1;
+
 
     public void TogglePause()
     {
@@ -20,35 +25,54 @@ public class GlobalPauseHandler : MonoBehaviour
 
     public void Pause()
     {
+        if (lockKey > -1)
+        {
+            Debug.Log("Pausing requested, but it is currently lock");
+            return;
+        }
+
         GlobalControl.Instance.paused = true;
         Time.timeScale = 0;
-
-        // Trigger Ball physics pause listener
         GameObject.Find("Ball").GetComponent<Kinematics>().TriggerPause();
-        Debug.Log("pause");
+
+        SetIndicatorVisibility(true);
     }
 
     public void Resume()
     {
+        if (lockKey > -1)
+        {
+            Debug.Log("Resuming requested, but it is currently lock");
+            return;
+        }
+
         GlobalControl.Instance.paused = false;
         Time.timeScale = GlobalControl.Instance.timescale;
-        // Trigger Ball physics resume listener
         GameObject.Find("Ball").GetComponent<Kinematics>().TriggerResume();
-        Debug.Log("Resume");
+        SetIndicatorVisibility(false);
     }
 
-    public void SetIndicatorVisibility(bool visible)
+    public int SetIndicatorVisibility(bool _visible, bool _generateLock = false, int _lockKey = -1)
     {
-        if (visible)
+        // If there is an activelock
+        if (lockKey > 0)
         {
-            pauseIndicator.visibleOverride = true;
-            pauseIndicator.visibleOverrideValue = true;
+            // Refuse the modification if the wrong key is provided
+            if (_lockKey != lockKey)
+            {
+                Debug.Log("Pause indicator requested, but it is currently lock");
+                return -1;
+            }
         }
-        else
+        lockKey = -1;
+
+        if (_generateLock)
         {
-            pauseIndicator.visibleOverride = true;
-            pauseIndicator.visibleOverrideValue = false;
+            lockKey = Random.Range(1, 1000);
         }
+
+        pauseIndicator.SetActive(_visible);
+        return lockKey;
     }
 }
 
