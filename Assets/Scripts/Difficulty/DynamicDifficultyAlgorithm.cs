8d574d8ea5637fb6aa2dbc060ec873bcaf249455
Pariterre <pariterre@hotmail.com>
2022-05-05 17:15:00 -0400

    Starting a simple DDA

---
+++ Assets/Scripts/Difficulty/DynamicDifficultyAlgorithm.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DynamicDifficultyAlgorithm
+{
+    public int ComputeNewLevel(DifficultyManager _difficulty, Trial _trial)
+    {
+        bool _trialSuccessful = _difficulty.AreTrialConditionsMet(_trial);
+        int _newLevel = -1;
+        if (_trialSuccessful)
+        {
+            if (_difficulty.currentLevel + 1 < _difficulty.nbLevel)
+                _newLevel = _difficulty.currentLevel + 1;
+        }
+        return _newLevel;
+    }
+}
