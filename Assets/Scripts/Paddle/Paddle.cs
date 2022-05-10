438f01426b2fdebca543c123fba216b0d21cdd8f
Pariterre <pariterre@hotmail.com>
2022-05-10 10:44:56 -0400

    Reinstated the two paddles logic

--- Assets/Scripts/Paddle/Paddle.cs
+++ Assets/Scripts/Paddle/Paddle.cs
@@ -8,36 +8,15 @@ public class Paddle : MonoBehaviour
     // Is this the left paddle or the right paddle?
     private PaddleIdentifier paddleIdentifier;
 
-    // The materials used to display this paddle. If it is transparent,
-    // disable the collider.
-    [SerializeField]
-    private Material opaquePaddleMat;
-    [SerializeField]
-    private Material opaqueBacksideMat;
-    [SerializeField]
-    private Material transparentPaddleMat;
-    [SerializeField]
-    private Material transparentBacksideMat;
-
     // The collider for this paddle
     [SerializeField]
     private GameObject paddleCollider;
-
-    // The mesh renderers for this paddle. Modify the material that they use.
-    [SerializeField]
-    private GameObject paddleModel;
-    [SerializeField]
-    private GameObject backsideModel;
-
     private PhysicsTracker m_MotionData = new PhysicsTracker();
 
     // Enable this paddle. Make it visible and turn on collider
     public void EnablePaddle()
     {
         paddleCollider.SetActive(true);
-
-        paddleModel.GetComponent<MeshRenderer>().material = opaquePaddleMat;
-        backsideModel.GetComponent<MeshRenderer>().material = opaqueBacksideMat;
 
         // Use UnityLabs PhysicsTracker
         m_MotionData.Reset(transform.position, transform.rotation, Vector3.zero, Vector3.zero);
@@ -49,13 +28,7 @@ public class Paddle : MonoBehaviour
 
     private void Start()
     {
-        opaquePaddleMat = GetComponent<Material>();
-        opaqueBacksideMat = GetComponent<Material>();
-        transparentPaddleMat = GetComponent<Material>();
-        transparentBacksideMat = GetComponent<Material>();
         paddleCollider = paddleCollider.gameObject;
-        paddleModel = paddleModel.gameObject;
-        backsideModel = backsideModel.gameObject;   
     }
 
     void Update()
@@ -68,9 +41,6 @@ public class Paddle : MonoBehaviour
     public void DisablePaddle()
     {
         paddleCollider.SetActive(false);
-
-        paddleModel.GetComponent<MeshRenderer>().material = transparentPaddleMat;
-        backsideModel.GetComponent<MeshRenderer>().material = transparentBacksideMat;
     }
 
     public Vector3 Position { get { return transform.position; } }
