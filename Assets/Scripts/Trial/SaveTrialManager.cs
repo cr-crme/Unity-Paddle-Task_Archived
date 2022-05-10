787df7ae490eed33b855cbcc5e38da9fe20d30a5
Pariterre <pariterre@hotmail.com>
2022-05-10 13:10:55 -0400

    Finalized writing to a file

--- Assets/Scripts/Trial/SaveTrialManager.cs
+++ Assets/Scripts/Trial/SaveTrialManager.cs
@@ -1,23 +1,32 @@
 ﻿using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
 
-public class SaveTrialManager
+public class SaveTrialManager : MonoBehaviour
 {
+    private string resultsFolder;
+
     private FileIO_CSV.CsvFileWriter trial;
     private FileIO_CSV.CsvRow trialCsv;
     public SaveTrialManager()
     {
-        trial = new FileIO_CSV.CsvFileWriter("result/Trial.csv");
+        // Create a results directory if it does not exist
+        resultsFolder = $"Results/{GlobalPreferences.Instance.participantID}";
+        Directory.CreateDirectory(resultsFolder);
 
-        trialCsv = new FileIO_CSV.CsvRow(new List<string>() { "Time", "Ball position" });
-        trial.Write(trialCsv);
+        // Prepare the Trial csv file
+        trial = new FileIO_CSV.CsvFileWriter($"{resultsFolder}/Trial.csv");
+        trialCsv = new FileIO_CSV.CsvRow(new List<string>() { "Time", "Ball position"});
+        trial.Write(trialCsv + "\n");
     }
 
     public void AddFrameToTrial(Trial _trial, Ball _ball)
     {
         trialCsv.SetColumns(new List<string>() { _trial.time.ToString(), _ball.height.ToString() });
-        trial.Write(trialCsv);
+        string tata = _trial.time.ToString();
+        trial.Write(trialCsv.ToString() + "\n");
     }
-    ~SaveTrialManager()
+    void OnDestroy()
     {
         trial.Close();
     }
